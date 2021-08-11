2447246ca8d1f01f68f02c93723090342957879e
Ligia <mktligia@gmail.com>
2021-08-11 13:15:20 -0400

    avances

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "lists#index" #cuando se ingresa a la URL principel, dibuje index del controller list index
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :movies, only: [:new, :create]
+  end
 end
