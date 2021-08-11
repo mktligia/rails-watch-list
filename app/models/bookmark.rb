80fe475f1b839899af5515aa8b4046de4a732a0f
Ligia <mktligia@gmail.com>
2021-08-11 11:41:23 -0400

    avances

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :list
   belongs_to :movie
 
+  validates :movie_id, uniqueness:{ scope: :list_id, message: "Ya se encuentra en la lista"}
+  validates :comment, length: { minimum: 6 }
+
 end
