2a03746b39db733294542dd45e90970d4a958972
Ligia <mktligia@gmail.com>
2021-08-13 09:44:54 -0400

    seed

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,41 @@
 class BookmarksController < ApplicationController
+  
+  def create
+    @comment = Comment.new(comment_params)
+      if @comment.save
+        redirect_to comment_path(@comment)
+      else
+        render :new
+    end
+    
+    def update
+    
+    end
+  end 
+  
+  
+  def new
+    @movie = Ta.new
+  end
+  
+  
+  
+  def index
+        @movie = Movie.all
+      end
+    
+      def show
+        @movie = Movie.find(params[:id])
+      end
+    
+    
+      
+      
+    
+      private
+    
+      def task_params
+        params.require(:task).permit(:title, :details, :completed)
+      end
+    
 end
